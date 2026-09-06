1657aed4ce6e7142d7159b459e3cde423b8fb409
Christian Fronk <cfronk12@gmail.com>
2026-09-05 23:40:48 -0400

    Update thresh

--- ros_handoff_detection/ros_handoff_detection/detect_handoff_node.py
+++ ros_handoff_detection/ros_handoff_detection/detect_handoff_node.py
@@ -71,7 +71,7 @@ class HandoffInferenceNode(Node):
         # adding only one frame of confirmation latency.
         self.declare_parameter(
             "handoff_confirmation_frames",
-            4,
+            3,
         )
 
         self.declare_parameter(
